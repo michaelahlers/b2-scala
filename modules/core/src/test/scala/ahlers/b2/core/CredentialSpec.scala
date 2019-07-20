b4dd8b1aeb6bad1699b04df39622316a54521e37
Michael Ahlers <michael@ahlers.consulting>
2019-07-20 11:30:19 -0500

    Update tests for deprecations in ScalaTest

--- modules/core/src/test/scala/ahlers/b2/core/CredentialSpec.scala
+++ modules/core/src/test/scala/ahlers/b2/core/CredentialSpec.scala
@@ -6,71 +6,73 @@ import cats.syntax.option._
 import org.scalacheck._
 import org.scalamock.scalatest._
 import org.scalatest.Matchers._
-import org.scalatest._
+import org.scalatest.wordspec._
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks._
 
 /**
  * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
  */
-class CredentialSpec extends FlatSpec with MockFactory {
+class CredentialSpec extends AnyWordSpec with MockFactory {
 
-  behavior of "Providers"
+  "Providers" can {
 
-  they can "get credentials directly" in {
-    import ScalacheckShapeless._
+    "get credentials directly" in {
+      import ScalacheckShapeless._
 
-    forAll { credential: Credential =>
-      Provider
-        .direct(credential.key, credential.secret)
-        .get() should contain(credential)
-    }
-  }
-
-  they can "get credentials from environment" in {
-    import ScalacheckShapeless._
-
-    forAll { (provider: Environment, credential: Credential) =>
-      org.joor.Reflect
-        .on("java.lang.ProcessEnvironment")
-        .set(
-          "theUnmodifiableEnvironment",
-          new java.util.HashMap[String, String]() {
-            putAll(System.getenv())
-            put(provider.key, credential.key)
-            put(provider.secret, credential.secret)
-          }
-        )
-
-      Provider
-        .environment(provider.key, provider.secret)
-        .get() should contain(credential)
-    }
-  }
-
-  they can "get when chained" in {
-    import Arbitrary._
-    import Gen._
-    import ScalacheckShapeless._
-
-    forAll(choose(0, 3), arbitrary[Credential], choose(0, 3)) { (heads, credential, tails) =>
-      val providers: Seq[Provider] = {
-        (Seq.fill(heads) {
-          val p = mock[Provider]
-          (p.get _).expects().once().returns(none)
-          p
-        } :+ {
-          val p = mock[Provider]
-          (p.get _).expects().once().returns(credential.some)
-          p
-        }) ++ Seq.fill(tails) {
-          val p = mock[Provider]
-          (p.get _).expects().never()
-          p
-        }
+      forAll { credential: Credential =>
+        Provider
+          .direct(credential.key, credential.secret)
+          .get() should contain(credential)
       }
-
-      (providers reduce { _ | _ }).get() should contain(credential)
     }
+
+    "get credentials from environment" in {
+      import ScalacheckShapeless._
+
+      forAll { (provider: Environment, credential: Credential) =>
+        org.joor.Reflect
+          .on("java.lang.ProcessEnvironment")
+          .set(
+            "theUnmodifiableEnvironment",
+            new java.util.HashMap[String, String]() {
+              putAll(System.getenv())
+              put(provider.key, credential.key)
+              put(provider.secret, credential.secret)
+            }
+          )
+
+        Provider
+          .environment(provider.key, provider.secret)
+          .get() should contain(credential)
+      }
+    }
+
+    "get when chained" in {
+      import Arbitrary._
+      import Gen._
+      import ScalacheckShapeless._
+
+      forAll(choose(0, 3), arbitrary[Credential], choose(0, 3)) { (heads, credential, tails) =>
+        val providers: Seq[Provider] = {
+          (Seq.fill(heads) {
+            val p = mock[Provider]
+            (p.get _).expects().once().returns(none)
+            p
+          } :+ {
+            val p = mock[Provider]
+            (p.get _).expects().once().returns(credential.some)
+            p
+          }) ++ Seq.fill(tails) {
+            val p = mock[Provider]
+            (p.get _).expects().never()
+            p
+          }
+        }
+
+        (providers reduce { _ | _ }).get() should contain(credential)
+      }
+    }
+
   }
 
 }
