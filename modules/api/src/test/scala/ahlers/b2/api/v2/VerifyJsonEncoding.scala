006f0cf2e3a46b77690729861bcafa05f86b717d
Michael Ahlers <michael@ahlers.consulting>
2019-12-06 12:18:08 -0500

    Revise tests verifying shape

--- modules/api/src/test/scala/ahlers/b2/api/v2/VerifyJsonEncoding.scala
+++ modules/api/src/test/scala/ahlers/b2/api/v2/VerifyJsonEncoding.scala
@@ -11,6 +11,7 @@ import org.scalatest.enablers._
 import org.scalatest.wordspec._
 import org.scalatestplus.scalacheck._
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 /**
  * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
@@ -46,29 +47,33 @@ trait VerifyJsonEncoding { this: AnyWordSpecLike =>
 
       forAll { accountAuthorization: AccountAuthorization =>
         inside(Json.parse(Encoding.write(accountAuthorization))) {
-          case x =>
-            (__ \ "absoluteMinimumPartSize").read[Long].reads(x) should contain(accountAuthorization.absoluteMinimumPartSize)
-            (__ \ "accountId").read[String].reads(x) should contain(accountAuthorization.accountId)
-            (__ \ "apiUrl").read[String].reads(x) should contain(accountAuthorization.apiUrl)
-            (__ \ "authorizationToken").read[String].reads(x) should contain(accountAuthorization.authorizationToken)
-            (__ \ "allowed" \ "bucketId").readNullable[String].reads(x) should contain(accountAuthorization.allowed.bucketId)
-            (__ \ "allowed" \ "bucketName").readNullable[String].reads(x) should contain(accountAuthorization.allowed.bucketName)
-            accountAuthorization.allowed.capabilities.zipWithIndex foreach {
-              case (DeleteBuckets, ci) => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("deleteBuckets")
-              case (DeleteFiles, ci)   => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("deleteFiles")
-              case (DeleteKeys, ci)    => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("deleteKeys")
-              case (ListBuckets, ci)   => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("listBuckets")
-              case (ListFiles, ci)     => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("listFiles")
-              case (ListKeys, ci)      => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("listKeys")
-              case (ReadFiles, ci)     => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("readFiles")
-              case (ShareFiles, ci)    => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("shareFiles")
-              case (WriteBuckets, ci)  => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("writeBuckets")
-              case (WriteFiles, ci)    => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("writeFiles")
-              case (WriteKeys, ci)     => (__ \ "allowed" \ "capabilities" \ ci).read[String].reads(x) should contain("writeKeys")
-            }
-            (__ \ "allowed" \ "namePrefix").readNullable[String].reads(x) should contain(accountAuthorization.allowed.namePrefix)
-            (__ \ "downloadUrl").read[String].reads(x) should contain(accountAuthorization.downloadUrl)
-            (__ \ "recommendedPartSize").read[Long].reads(x) should contain(accountAuthorization.recommendedPartSize)
+          import AccountAuthorization.Allowed
+ PartialFunction.fromFunction(   ((__ \ "absoluteMinimumPartSize").read[Long] and
+            (__ \ "accountId").read[String] and
+            (__ \ "allowed").read(              ((__ \ "capabilities").read(Reads.seq(Reads[Capability] {
+                case JsString("deleteBuckets") => JsSuccess(DeleteBuckets)
+                case JsString("deleteFiles")   => JsSuccess(DeleteFiles)
+                case JsString("deleteKeys")    => JsSuccess(DeleteKeys)
+                case JsString("listBuckets")   => JsSuccess(ListBuckets)
+                case JsString("listFiles")     => JsSuccess(ListFiles)
+                case JsString("listKeys")      => JsSuccess(ListKeys)
+                case JsString("readFiles")     => JsSuccess(ReadFiles)
+                case JsString("shareFiles")    => JsSuccess(ShareFiles)
+                case JsString("writeBuckets")  => JsSuccess(WriteBuckets)
+                case JsString("writeFiles")    => JsSuccess(WriteFiles)
+                case JsString("writeKeys")     => JsSuccess(WriteKeys)
+                case capability                => JsError(s"""Unknown capability "$capability".""")
+              })) and
+                (__ \ "bucketId").readNullable[String] and
+                (__ \ "bucketName").readNullable[String] and
+                (__ \ "namePrefix").readNullable[String])
+                .apply(Allowed.apply _)) and
+            (__ \ "apiUrl").read[String] and
+            (__ \ "authorizationToken").read[String] and
+            (__ \ "downloadUrl").read[String] and
+            (__ \ "recommendedPartSize").read[Long])
+            .apply(AccountAuthorization.apply _)
+            .reads(_) should matchTo(JsSuccess(accountAuthorization): JsResult[AccountAuthorization]))
         }
       }
     }
